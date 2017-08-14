d08c650c2a3c4a50492bebde2a5ac1d24e109c7f
Nikita Belonogov <belonogovna@sgu.ru>
2017-08-14 11:57:14 +0400

    Add punctuation processing

--- test/test_dialog_engine.py
+++ test/test_dialog_engine.py
@@ -52,6 +52,50 @@ class TestDialogEngine(unittest.TestCase):
             print(k, v, answer)
             self.assertTrue(answer in v)
 
+    def test_some_answers_with_punctuation(self):
+        ls = {
+            'привет.': [
+                'ну привет',
+                'тебя не звали, иди отсюда'
+            ],
+            'пока!': [
+                'ну пока',
+                'ну и пиздуй'
+            ],
+            'пизда.': [
+                'не ругайся!'
+            ],
+            'да!': [
+                'манда',
+                'хуй на'
+            ],
+            'нет.': [
+                'пидора ответ'
+            ],
+            'пидор!': [
+                'пидор дед твой',
+                'ты сам пидор',
+            ],
+            'сидор.': [
+                'это я люблю',
+            ],
+            'го.': [
+                'го по пиву лучше '
+            ],
+            'го гулять!': [
+                'го по пиву лучше '
+            ],
+            '))))!': [
+                '))))))))))))',
+                'гы))))'
+            ]
+        }
+
+        for k, v in ls.items():
+            answer = self.de.choose_answer(k)
+            print(k, v, answer)
+            self.assertTrue(answer in v)
+
     def test_some_case_insensitive(self):
         ls = {
             'Привет': [
